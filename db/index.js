e0e8f80cf5e2c5acd8e83393f96f447df1c5eb29
Success96 <lfawas6@gmail.com>
2022-11-16 11:20:39 +0100

    Update the DB conection method

--- db/index.js
+++ db/index.js
@@ -1,20 +1,21 @@
-const { MongoClient } = require("mongodb");
 require ("dotenv").config();
-const Mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const { MONGO_URI } = process.env;
 
-
-const {URI} = process.env;
-const client = Mongoose.connect(URI);
+//onst client = mongoose.connect(URI);
 async function connectDB() {
     try {
-        await Mongoose.connect(URI);
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+           // useCreateIndex: true,
+            useUnifiedTopology: true,
+           // useFindAndModify: false,
+        });
         console.log("Connected correctly to DB");
     } catch (err) {
         console.log(err.message);
-    }
-    // finally {
-    //     await client.close();
-    // }
+        await process.exit(1)
+    } 
 }
 // run().catch(console.dir);
 
